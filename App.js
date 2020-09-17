07c7d21b04995ffe074ecebb71e5b73efe900a27
Carlos.Inhani <carlos.inhani@gmail.com>
2020-09-17 10:56:45 -0300

    Application structure

--- App.js
+++ App.js
@@ -1,21 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Fragment} from 'react';
+import { StyleSheet, Text, View, SafeAreaView, ScrollView, Button, FlatList } from 'react-native';
 
-export default function App() {
+const App = () => {
+ const addEntry = () => {
+    alert ("Abrir tela de adicionar lançamento.");
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{padding: 10}}>
+      <Text style={{fontSize: 22, fontWeight: "bold", marginTop: 10, marginBottom: 10}}>Saldo: R$2.102,45</Text>
+      <Button onPress={addEntry} title="Adicionar"></Button>
+
+      <Text style={{fontSize: 22, fontWeight: "bold", marginTop: 10, marginBottom: 10}}>Categorias</Text>
+
+      <FlatList data={[
+        {key: "Alimentação: R$200"},
+        {key: "Combustível: R$12"},
+        {key: "Aluguel: R$120"},
+        {key: "Lazer: R$250"},
+        {key: "Outros: R$1200"},
+      ]}
+    renderItem={({item}) => <Text>{item.key}</Text> }
+      ></FlatList>
+
+      <Text style={{fontSize: 22, fontWeight: "bold", marginTop: 10, marginBottom: 10}}>Ultimos Lançamentos</Text>
+
+      <FlatList data={[
+        {key: "Padaria Asa Branca: R$10"},
+        {key: "Supermercado Isadora: R$190"},
+        {key: "Posto Ipiranga: R$190"},
+      ]}
+      renderItem={({item}) => <Text>{item.key}</Text> }
+      ></FlatList>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
+
 });
+
+export default App;
